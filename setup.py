481e8d38d64f1ea3616c3169e2b69a601fdd0e30
Adewusi Akeem Olumide <olumide_adewusi@yahoo.com>
2026-04-07 02:41:08 +0100

    Add requirement.txt file and dependencies

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Adewusi Akeem Olumide",
+    author_email="olumide_adewusi@yahoo.com",
+    packages=find_packages(),
+    install_requires=[]
+)
